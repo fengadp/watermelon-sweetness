98f4bf6e844b2c9e6b24bf07240d4d1be99f7716
fengadp <88819531+fengadp@users.noreply.github.com>
2026-04-22 21:57:50 +0700

    Update mic.py

--- mic.py
+++ mic.py
@@ -30,18 +30,19 @@ def audioPreprocessing(data):
     x = data
     x_abs = np.abs(x)
     n_points = len(x_abs)
-    #threshold = 5000
-    x_abs_sorted = np.argsort(x_abs)
-    x_abs_reverse = np.flip(x_abs_sorted)
-    index_max = np.sort(x_abs_reverse[0:numSignals_max])
-    threshold = x_abs[index_max]
+    threshold = 5000
+    
+    #x_abs_sorted = np.argsort(x_abs)
+    #x_abs_reverse = np.flip(x_abs_sorted)
+    #index_max = np.sort(x_abs_reverse[0:numSignals_max])
+    #threshold = x_abs[index_max]
     
     pos = 0
     for k in range(numSignals_max):
         if n_points > signal_len:
             for i in range(n_points):
                 if i >= signalOffset:
-                    if (x_abs[i] == threshold[k]):
+                    if (x_abs[i] == threshold):
                         pos = i
                         #print(pos)
                         # + 10 do the same as student project
@@ -79,7 +80,7 @@ audio_bytes = audio_recorder(
     #icon_size="4x",
     energy_threshold=(-1.0,1.0),
     pause_threshold=4.0,
-    sample_rate=16000,
+    sample_rate=44100,
 )
 st.write("and then knock on the watermelon three times")
 
