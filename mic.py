7d60943045435c10b6ffdd52be6124e1d11c7dad
fengadp <88819531+fengadp@users.noreply.github.com>
2026-04-22 16:43:10 +0700

    Update mic.py

--- mic.py
+++ mic.py
@@ -35,13 +35,13 @@ def audioPreprocessing(data):
     x_abs_reverse = np.flip(x_abs_sorted)
     index_max = np.sort(x_abs_reverse[0:numSignals_max])
     threshold = x_abs[index_max]
-    print(threshold)
+    
     pos = 0
     for k in range(numSignals_max):
         if n_points > signal_len:
             for i in range(n_points):
                 if i >= signalOffset:
-                    if (x_abs[i] >= threshold[k]):
+                    if (x_abs[i] == threshold[k]):
                         pos = i
                         #print(pos)
                         # + 10 do the same as student project
