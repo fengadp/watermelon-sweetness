1b047105a54c18063f44e4469e47674c6dac6d1e
fengadp <88819531+fengadp@users.noreply.github.com>
2026-04-22 16:38:18 +0700

    Update mic.py

--- mic.py
+++ mic.py
@@ -30,12 +30,13 @@ def audioPreprocessing(data):
     x = data
     x_abs = np.abs(x)
     n_points = len(x_abs)
+    #threshold = 5000
     x_abs_sorted = np.argsort(x_abs)
     x_abs_reverse = np.flip(x_abs_sorted)
     index_max = np.sort(x_abs_reverse[0:numSignals_max])
     threshold = x_abs[index_max]
+    print(threshold)
     pos = 0
-    
     for k in range(numSignals_max):
         if n_points > signal_len:
             for i in range(n_points):
