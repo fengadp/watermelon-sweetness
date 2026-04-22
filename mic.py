1282733fe9a9e45120d4585fc04b00e9f7772423
fengadp <88819531+fengadp@users.noreply.github.com>
2026-04-22 22:26:15 +0700

    Update mic.py

--- mic.py
+++ mic.py
@@ -114,7 +114,7 @@ if audio_bytes:
         X_test = X_test.reshape((X_test.shape[0], time_steps, X_test.shape[1]))
         Y_test = model.predict(X_test)
         Y_brix = model2.predict(X_test)
-        brix = np.mean(Y_brix) 
+        brix = np.max(Y_brix) 
         #print(Y_test)
         progress_bar.progress(40, text=progress_text)
 
